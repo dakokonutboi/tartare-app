645a94f761b90bd75c107a4f50e703dc0dc95b44
dakokonutboi <ghaliomar.g.ink@gmail.com>
2020-05-21 18:08:34 +0000

    Small edit

--- genkeys.js
+++ genkeys.js
@@ -6,7 +6,11 @@ function goback(){
 
 function makekeys(){
   displayer = document.getElementById('displayer')
-  displayer.innerHTML = "<h1>Generating...</h1><p>Wait while we generate your keys.<br>When the generation is done, you will be logged in automatically.</p>"
+  displayer.innerHTML = "<h1>Generating...</h1><p>Please wait while we generate your keys.<br>When the generation is done, you will be logged in automatically.</p>"
   child_process('tartarelib/generatersa.exe')
+  setTimeout(done, 2000)
+}
+
+function done(){
   displayer.innerHTML = "<h1>Done.</h1><p>Your keys were generated, we're logging you in...</p>"
 }
--- mainer.js
+++ mainer.js
@@ -4,7 +4,7 @@ let remember = document.getElementById('remember')
 
 if (fs.existsSync('tartare_private_key.pem')) {
   console.log('Private key exists')
-  remember.innerHTML = '<button type="button" class="button" name="button">Use saved key</button>'
+  remember.innerHTML = '<button type="button" class="button" name="button">Use existing key</button>'
 } else {
   console.log('does not')
 }
