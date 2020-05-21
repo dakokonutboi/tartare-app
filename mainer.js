90ea75a14764a25b777f4bab69d0ffa28387db92
dakokonutboi <ghaliomar.g.ink@gmail.com>
2020-05-21 17:36:43 +0000

    Existing key detection

---
+++ mainer.js
@@ -0,0 +1,10 @@
+const fs = require('electron').remote.require('fs')
+
+let remember = document.getElementById('remember')
+
+if (fs.existsSync('tartarelib/tartare_private_key.pem')) {
+  console.log('Private key exists')
+  remember.innerHTML = '<button type="button" class="button" name="button">Use saved key</button>'
+} else {
+  console.log('does not')
+}
