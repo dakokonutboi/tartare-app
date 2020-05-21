26626393679c38ed7c5bf0dee88276cf3b28eadc
dakokonutboi <ghaliomar.g.ink@gmail.com>
2020-05-21 17:59:22 +0000

    Tweaked the key generation

--- genkeys.js
+++ genkeys.js
@@ -8,4 +8,5 @@ function makekeys(){
   displayer = document.getElementById('displayer')
   displayer.innerHTML = "<h1>Generating...</h1><p>Wait while we generate your keys.<br>When the generation is done, you will be logged in automatically.</p>"
   child_process('tartarelib/generatersa.exe')
+  displayer.innerHTML = "<h1>Done.</h1><p>Your keys were generated, we're logging you in...</p>"
 }
